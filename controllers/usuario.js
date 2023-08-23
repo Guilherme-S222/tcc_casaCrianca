b968b6ebab73d9dda2c31371215ab4fae7ff94c2
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2023-08-22 22:45:11 -0300

    aula 22-08-2023

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -15,7 +15,18 @@ module.exports = {
     },
     async cadastrarUsuario(request, response){
         try {
-            return response.status(200).json({confirma:'cadastrarUsuário'});
+                //parâmetros passados via corpo da requisição
+            const { user_nome,user_senha,insti_id_user } = request.body;
+                //instrução sql para inserção
+            const sql = 'INSERT INTO usuario (user_nome, user_senha, insti_id_user) VALUES (?, ?, ?)';
+                //definição de array com os parâmetros que receberam os valores do front-end
+            const values = [user_nome, user_senha, insti_id_user];
+                //executa a instrução de inserção no banco de dados
+            const confirmacao = await db.query(sql, values);
+                //Exibe o id do registro inserido
+            const user_id = confirmacao[0].insertId;
+                //mensagem de retorno no formato JSON
+            return response.status(200).json({confirma:'Cadastro de usuário realizado com sucesso', message: user_id});
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
         }
